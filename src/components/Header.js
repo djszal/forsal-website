e048f148ec9347ce419a57fc220df32be555f398
Dylan <szalank.dj@gmail.com>
2023-06-02 16:40:51 -0400

    added header links to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 const Header = () => {
     return(
-        <h1>Hello</h1>
+        <div className='header'>
+        <div className='app-title'><Link to={"/"} className="header-links">JobTrax</Link></div>
+
+        <div className='nav-links'>
+          <div className='links'><Link to={"/estimate"} className="header-links">Updates</Link></div>
+          <div className='links'><Link to={"/"} className="header-links">About Us</Link></div>
+          <div className='links'><Link to={"/"} className="header-links">Services</Link></div>
+          <div className='links'><Link to={"/"} className="header-links">Gallery</Link></div>        
+          <div className='links'><Link to={"/"} className="header-links">Contact Us</Link></div>        
+          <div className='links'><Link to={"/"} className="header-links">Get Quote</Link></div>        
+        
+        </div>
+      </div>
     )
 
 }
