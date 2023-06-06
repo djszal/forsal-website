98f23ae0d1bd96d7f926e47de0a6397c0ca5416b
Dylan <szalank.dj@gmail.com>
2023-06-05 20:31:18 -0400

    added Header to App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
        
           Learn React
         
-      </header>
+    
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom";
 const Header = () => {
     return(
         <div className='header'>
-        <div className='app-title'><Link to={"/"} className="header-links">JobTrax</Link></div>
+        <div className='app-title'><Link to={"/"} className="header-links">Forsal Construction</Link></div>
 
         <div className='nav-links'>
-          <div className='links'><Link to={"/estimate"} className="header-links">Updates</Link></div>
+          <div className='links'><Link to={"/"} className="header-links">Updates</Link></div>
           <div className='links'><Link to={"/"} className="header-links">About Us</Link></div>
           <div className='links'><Link to={"/"} className="header-links">Services</Link></div>
           <div className='links'><Link to={"/"} className="header-links">Gallery</Link></div>        
