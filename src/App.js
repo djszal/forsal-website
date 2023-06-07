a37e0b5cac6e99af0edc7c7222daab64a4dcf183
Dylan <szalank.dj@gmail.com>
2023-06-06 20:19:12 -0400

    fixed error with Link on Header and added Home component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-       
-          Learn React
+    
+          <div>
+            <Routes>
+              <Route exact path='/' element={<Home />}></Route>
+            </Routes>
+          </div>
         
     
     </div>
