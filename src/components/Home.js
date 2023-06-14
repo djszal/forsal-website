d8aa315dfe437a84281abdf637cfcc9366b40e62
Dylan <szalank.dj@gmail.com>
2023-06-13 21:18:31 -0400

    worked on Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,11 @@ import React from "react";
 const Home = () => {
     return(
         <div className='home'>
+            <image></image>
             <h1>Forsal Construction</h1>
             <h3>General Contractor & Handyman Services in the Greater Richmond Metro Area</h3>
-            <button>GET A QUOTE</button>
+            <button>GET QUOTE</button>
+            <img src=""/>
         
       </div>
     )
